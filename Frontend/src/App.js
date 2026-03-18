da29376c805fd0a880464a9fcae6098b0691f284
Eung-Seok <rwjflrk1234@gmail.com>
2026-03-18 14:42:17 +0900

    퀘스트 목록, 내 퀘스트 페이지 생성

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/login/Login';
 import SignUp from './pages/auth/signup/SignUp';
 import Terms from './pages/auth/signup/Terms';
+import QuestList from './pages/quest/QuestList/QuestList';
+import MyQuest from './pages/quest/MyQuest/MyQuest';
+import QuestDetail from './pages/quest/QuestDetail/QuestDetail';
 
 function App() {
   return (
@@ -12,7 +14,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/terms" element={<Terms />} />
-        {/* 기본 경로 설정 */}
+        <Route path="/explore" element={<QuestList />} />
+        <Route path="/quest-list" element={<QuestList />} />
+        <Route path="/quest/:questId" element={<QuestDetail />} />
+        <Route path="/quest" element={<MyQuest />} />
+        <Route path="/myquest" element={<MyQuest />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </Router>
